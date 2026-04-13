5347c1e28beff90d16884d32aa47a522c8a3097c
efriedman4 <efriedman4@huskers.unl.edu>
2026-04-12 22:19:33 -0500

    improved the encoding and features

--- decisionTree_reg.py
+++ decisionTree_reg.py
@@ -4,20 +4,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 cab_rides_and_weather = pd.read_csv('sample_cab_rides_and_weather.csv')
-le = LabelEncoder()
+cab_rides_and_weather = pd.get_dummies(
+    cab_rides_and_weather,
+    columns=['name', 'source', 'destination']
+)
 
-cab_rides_and_weather['name_encoded'] = le.fit_transform(cab_rides_and_weather['name'])
-cab_rides_and_weather['source_encoded'] = le.fit_transform(cab_rides_and_weather['source'])
-cab_rides_and_weather['destination_encoded'] = le.fit_transform(cab_rides_and_weather['destination'])
-
-X = cab_rides_and_weather[['distance', 
-                           'temp', 
-                           'rain', 
-                           'wind', 
-                           'surge_multiplier',
-                           'name_encoded',
-                           'source_encoded',
-                           'destination_encoded']]
+X = cab_rides_and_weather.drop(columns=['price', 'surge_multiplier'])
 y = cab_rides_and_weather['price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
--- svr.py
+++ svr.py
@@ -5,12 +5,12 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 cab_rides_and_weather = pd.read_csv('sample_cab_rides_and_weather.csv')
 
-le = LabelEncoder()
-cab_rides_and_weather['name_encoded'] = le.fit_transform(cab_rides_and_weather['name'])
-cab_rides_and_weather['source_encoded'] = le.fit_transform(cab_rides_and_weather['source'])
-cab_rides_and_weather['destination_encoded'] = le.fit_transform(cab_rides_and_weather['destination'])
+cab_rides_and_weather = pd.get_dummies(
+    cab_rides_and_weather,
+    columns=['name', 'source', 'destination']
+)
 
-X = cab_rides_and_weather[['distance', 'temp', 'rain', 'wind', 'surge_multiplier', 'name_encoded', 'source_encoded', 'destination_encoded']]
+X = cab_rides_and_weather.drop(columns=['price', 'surge_multiplier'])
 y = cab_rides_and_weather['price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
